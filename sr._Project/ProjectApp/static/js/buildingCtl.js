736745970ed45af5cec1f658f3c0b38fb34ee852
flysaiah <mayeis01@yahoo.com>
2016-11-28 23:59:19 -0600

    Most front-end registration changes made

--- sr._Project/ProjectApp/static/js/buildingCtl.js
+++ sr._Project/ProjectApp/static/js/buildingCtl.js
@@ -1,6 +1,6 @@
 // For each individual building
-var app = angular.module("building", ["services"]);
-app.controller('buildingCtl', function($scope, $location, getFloorInfo) {
+var app = angular.module("building", ["ngMaterial", "services"]);
+app.controller('buildingCtl', function($scope, $location, getFloorInfo, getRoomInfo, registrationService, $mdSidenav) {
   // get name of building from query parameter
   $scope.buildingName = $location.search()["bname"];
 
@@ -9,4 +9,28 @@ app.controller('buildingCtl', function($scope, $location, getFloorInfo) {
     $scope.floorList = res.floorList;
   });
 
+  $scope.toggleRight = function(roomNum1, roomNum2) {
+    $scope.roomNumber = roomNum1.toString() + roomNum2.toString();
+    $scope.headerTitle = $scope.buildingName + " " + $scope.roomNumber;
+
+    getRoomInfo.getOccupants($scope.buildingName, $scope.roomNumber).then(function(res) {
+      $scope.currentOccupants = res;
+    });
+    $mdSidenav('right').toggle();
+  }
+
+  $scope.registerForRoom = function() {
+    registrationService.registerForRoom($scope.groupID, $scope.buildingName, $scope.roomNumber).then(function(res) {
+      if (!res.wasSuccessful) {
+        console.log("ERROR registering for room");
+      }
+    })
+  }
+
+})
+.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function () {
+    // Component lookup should always be available since we are not using `ng-if`
+    $mdSidenav('right').close();
+  };
 });
